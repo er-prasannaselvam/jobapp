bd7dcd040149fd47b632be3186f37806721aa415
er-prasannaselvam <selvamprasanna2002@gmail.com>
2025-04-28 21:06:56 +0530

    file updated

--- src/main/java/com/springboot/jobapp/JobAppApplication.java
+++ src/main/java/com/springboot/jobapp/JobAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobAppApplication.class, args);
 	}
