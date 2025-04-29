b58f3fadc068bc855383472f7dcf6e6623acefdb
er-prasannaselvam <selvamprasanna2002@gmail.com>
2025-04-29 19:49:13 +0530

    file updated

--- src/main/java/com/springboot/jobapp/model/JobPost.java
+++ src/main/java/com/springboot/jobapp/model/JobPost.java
@@ -1,26 +1,25 @@
 package com.springboot.jobapp.model;
 
+
 import java.util.List;
 
 import org.springframework.stereotype.Component;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Component
 public class JobPost {
 
-    private int PostID;
-    private String PostProfile;
-    private String PostDesc;
-    private int ReqExperience;
-    private List<String> PostTechStack;
-    
 
-    
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private Integer reqExperience;
+	private List<String> postTechStack;
+	
+
 }
